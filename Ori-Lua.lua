bc0a21f67e498af2949a0515a87dea6cc92028b6
LunarLuzogSM <54291915+LunarLuzogSM@users.noreply.github.com>
2020-03-03 21:53:44 +0100

    Create Ori-Lua.lua

---
+++ Ori-Lua.lua
@@ -0,0 +1,11 @@
+-- Ori-Lua --
+-- Made By LunarLuzog Aka ShiinaChan --
+
+------------------------------------
+print("Successfully Loaded Ori-Lua")
+------------------------------------
+local CURRENTVERSION = "1.0"
+local LATESTVERSION = http.Get("https://raw.githubusercontent.com/LunarLuzogSM/OriLua/master/version.txt")
+------------------------------------
+local OriLua_WINDOW = gui.Window("orilua", "Ori-Lua", 100, 100, 500, 500)
+------------------------------------
