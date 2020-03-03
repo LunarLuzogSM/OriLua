f7ccaae3e759e7bcf4fd2861671cd7d9708e997d
LunarLuzogSM <54291915+LunarLuzogSM@users.noreply.github.com>
2020-03-03 22:12:08 +0100

    Update OriLua.lua

--- OriLua.lua
+++ OriLua.lua
@@ -1,9 +1,9 @@
 -- OriLua --
 -- Made By LunarLuzog Aka ShiinaChan --
 
-------------------------------------
+--------------------------------------
 print("Successfully Loaded OriLua")
-------------------------------------
+--------------------------------------
 local CURRENTVERSION = "1.0"
 local LATESTVERSION = http.Get("https://raw.githubusercontent.com/LunarLuzogSM/OriLua/master/version.txt")
 local function Update() 
@@ -14,9 +14,9 @@ local function Update()
         LoadScript(GetScriptName())
     end
 end
-------------------------------------
+--------------------------------------
 local OriLua_WINDOW = gui.Window("orilua", "OriLua", 100, 100, 500, 500)
-------------------------------------
+--------------------------------------
 local OriLua_TAB = gui.Tab(gui.Reference("Settings"), "orilua.updater", "OriLua Updater")
 local OriLua_UPDATER_GBOX = gui.Groupbox(OriLua_TAB, "Updater", 10, 10, 160, 0)
 local OriLua_UPDATER_CURRENTVERSION = gui.Text(OriLua_UPDATER_GBOX, "Current version: v" .. CURRENTVERSION)
@@ -24,4 +24,4 @@ local OriLua_UPDATER_LATESTVERSION = gui.Text(OriLua_UPDATER_GBOX, "Latest versi
 local OriLua_UPDATER_UPDATE = gui.Button(OriLua_UPDATER_GBOX, "Update", Update)
 local OriLua_UPDATER_CHANGELOG_GBOX = gui.Groupbox(OriLua_TAB, "Changelog", 190, 10, 360, 0)
 local OriLua_UPDATER_CHANGELOG_TEXT = gui.Text(OriLua_UPDATER_CHANGELOG_GBOX, http.Get("https://raw.githubusercontent.com/LunarLuzogSM/OriLua/master/changelog.txt"))
-------------------------------------
+--------------------------------------
