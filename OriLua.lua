daa6304bd7bb8cdef2391609c9491952e9408a46
LunarLuzogSM <54291915+LunarLuzogSM@users.noreply.github.com>
2020-03-05 21:51:54 +0100

    Update OriLua.lua

--- OriLua.lua
+++ OriLua.lua
@@ -2,8 +2,11 @@
 -- Made By LunarLuzog Aka ShiinaChan#5523 --
 
 -------------------------------------- Print
+
 print("Successfully Loaded OriLua")
+
 -------------------------------------- Auto Update
+
 local CURRENTVERSION = "1.0"
 local LATESTVERSION = http.Get("https://raw.githubusercontent.com/LunarLuzogSM/OriLua/master/version.txt")
 local function Update() 
@@ -14,7 +17,9 @@ local function Update()
         LoadScript(GetScriptName())
     end
 end
--------------------------------------- Gui
+
+-------------------------------------- Gui + Auto Update + Changelog
+
 local OriLua_TAB = gui.Tab(gui.Reference("Settings"), "orilua.LL", "OriLua LL")
 local OriLua_LL_UPDATER_GBOX = gui.Groupbox(OriLua_TAB, "Updater", 10, 10, 160, 0) -- Updater-GBOX
 local OriLua_LL_CHANGELOG_GBOX = gui.Groupbox(OriLua_TAB, "Changelog", 190, 10, 290, 0) -- Changelog-GBOX
@@ -23,4 +28,32 @@ local OriLua_LL_CURRENTVERSION = gui.Text(OriLua_LL_UPDATER_GBOX, "Current versi
 local OriLua_LL_LATESTVERSION = gui.Text(OriLua_LL_UPDATER_GBOX, "Latest version: v" .. LATESTVERSION)
 local OriLua_LL_UPDATE = gui.Button(OriLua_LL_UPDATER_GBOX, "Update", Update)
 local OriLua_LL_CHANGELOG_TEXT = gui.Text(OriLua_LL_CHANGELOG_GBOX, http.Get("https://raw.githubusercontent.com/LunarLuzogSM/OriLua/master/changelog.txt"))
---------------------------------------
+
+-------------------------------------- FullBright (adrianobessa5682 -- https://aimware.net/forum/user-236354.html)
+
+local client_SetConVar = client.SetConVar
+local OriLua_LL_VISUALS_FULLBRIGHT = gui.Checkbox(OriLua_LL_VISUALS_GBOX, "fulbright", "Full Bright", false)
+function full_bright()
+     if OriLua_LL_VISUALS_FULLBRIGHT:GetValue() then client_SetConVar("mat_fullbright", 1, true);
+        else 
+     client_SetConVar("mat_fullbright", 0, true);
+     end 
+    end
+callbacks.Register('Draw', "OriLua_LL_VISUALS_FULLBRIGHT" ,full_bright);
+
+-------------------------------------- Night Mode (adrianobessa5682 -- https://aimware.net/forum/user-236354.html)
+
+local OriLua_LL_VISUALS_EXPOSURE = gui.Slider(OriLua_LL_VISUALS_GBOX, "nex_bloom_exposure", "Night Mode", 100, 1, 100);
+callbacks.Register("Draw", "Night Mode", function()
+local controller = entities.FindByClass("CEnvTonemapController")[1];
+if(controller) then
+controller:SetProp("m_bUseCustomAutoExposureMin", 1);
+controller:SetProp("m_bUseCustomAutoExposureMax", 1);
+controller:SetProp("m_flCustomAutoExposureMin", OriLua_LL_VISUALS_EXPOSURE:GetValue()/100);
+controller:SetProp("m_flCustomAutoExposureMax", OriLua_LL_VISUALS_EXPOSURE:GetValue()/100);
+end
+end)
+
+-------------------------------------- Description
+
+OriLua_LL_VISUALS_EXPOSURE:SetDescription("100  Is  Lighter  Than  1")
