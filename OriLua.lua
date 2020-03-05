4af84c2931296554504c4a3f2d72c575daa355c9
LunarLuzogSM <54291915+LunarLuzogSM@users.noreply.github.com>
2020-03-05 21:20:09 +0100

    Update OriLua.lua

--- OriLua.lua
+++ OriLua.lua
@@ -16,11 +16,11 @@ local function Update()
 end
 -------------------------------------- Gui
 local OriLua_TAB = gui.Tab(gui.Reference("Settings"), "orilua.LL", "OriLua LL")
-local OriLua_LL_GBOX = gui.Groupbox(OriLua_TAB, "Updater", 10, 10, 160, 0)
-local OriLua_LL_CURRENTVERSION = gui.Text(OriLua_LL_GBOX, "Current version: v" .. CURRENTVERSION)
-local OriLua_LL_LATESTVERSION = gui.Text(OriLua_LL_GBOX, "Latest version: v" .. LATESTVERSION)
-local OriLua_LL_UPDATE = gui.Button(OriLua_LL_GBOX, "Update", Update)
-local OriLua_LL_CHANGELOG_GBOX = gui.Groupbox(OriLua_TAB, "Changelog", 190, 10, 290, 0)
-local OriLua_LL_ORILUA_GBOX = gui.Groupbox(OriLua_TAB, "Miscs", 10, 190, 470, 0)
+local OriLua_LL_UPDATER_GBOX = gui.Groupbox(OriLua_TAB, "Updater", 10, 10, 160, 0) -- Updater-GBOX
+local OriLua_LL_CHANGELOG_GBOX = gui.Groupbox(OriLua_TAB, "Changelog", 190, 10, 290, 0) -- Changelog-GBOX
+local OriLua_LL_VISUALS_GBOX = gui.Groupbox(OriLua_TAB, "VISUALS", 10, 190, 470, 0) -- Visuals-GBOX
+local OriLua_LL_CURRENTVERSION = gui.Text(OriLua_LL_UPDATER_GBOX, "Current version: v" .. CURRENTVERSION)
+local OriLua_LL_LATESTVERSION = gui.Text(OriLua_LL_UPDATER_GBOX, "Latest version: v" .. LATESTVERSION)
+local OriLua_LL_UPDATE = gui.Button(OriLua_LL_UPDATER_GBOX, "Update", Update)
 local OriLua_LL_CHANGELOG_TEXT = gui.Text(OriLua_LL_CHANGELOG_GBOX, http.Get("https://raw.githubusercontent.com/LunarLuzogSM/OriLua/master/changelog.txt"))
 --------------------------------------
