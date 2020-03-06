3a9f52023e0e3ca417042b1d726a66201739ec5e
LunarLuzog Aka ShiinaChan <54291915+LunarLuzogSM@users.noreply.github.com>
2020-03-06 12:49:34 +0100

    Update OriLua.lua

--- OriLua.lua
+++ OriLua.lua
@@ -24,18 +24,20 @@ local OriLua_TAB = gui.Tab(gui.Reference("Settings"), "orilua.LL", "OriLua LL")
 local OriLua_LL_UPDATER_GBOX = gui.Groupbox(OriLua_TAB, "Updater", 10, 10, 160, 0) -- Updater-GBOX
 local OriLua_LL_CHANGELOG_GBOX = gui.Groupbox(OriLua_TAB, "Changelog", 190, 10, 290, 0) -- Changelog-GBOX
 local OriLua_LL_VISUALS_GBOX = gui.Groupbox(OriLua_TAB, "Visuals", 10, 190, 470, 0) -- Visuals-GBOX
--- local OriLua_LL_MISCS_GBOX = gui.Groupbox(OriLua_TAB, "Miscs", 10, 350, 470, 0) -- Miscs-GBOX
+local OriLua_LL_HVH_GBOX = gui.Groupbox(OriLua_TAB, "HvH", 10, 435, 470, 0) -- HvH-GBOX
 local OriLua_LL_CURRENTVERSION = gui.Text(OriLua_LL_UPDATER_GBOX, "Current version: v" .. CURRENTVERSION)
 local OriLua_LL_LATESTVERSION = gui.Text(OriLua_LL_UPDATER_GBOX, "Latest version: v" .. LATESTVERSION)
 local OriLua_LL_UPDATE = gui.Button(OriLua_LL_UPDATER_GBOX, "Update", Update)
 local OriLua_LL_CHANGELOG_TEXT = gui.Text(OriLua_LL_CHANGELOG_GBOX, http.Get("https://raw.githubusercontent.com/LunarLuzogSM/OriLua/master/changelog.txt"))
 
--------------------------------------- Checkbox + Sliders
+-------------------------------------- Checkbox + Sliders / Visuals
 
-local OriLua_LL_VISUALS_FULLBRIGHT = gui.Checkbox(OriLua_LL_VISUALS_GBOX, "fulbright", "Full Bright", false)
+local OriLua_LL_VISUALS_FULLBRIGHT = gui.Checkbox(OriLua_LL_VISUALS_GBOX, "OriLua_LL_VISUALS_FULLBRIGHT", "Full Bright", false)
 local OriLua_LL_VISUALS_ASPECT_RATIO = gui.Checkbox(OriLua_LL_VISUALS_GBOX, "OriLua_LL_VISUALS_ASPECT_RATIO", "Aspect Ratio", false);  
-local OriLua_LL_VISUALS_EXPOSURE = gui.Slider(OriLua_LL_VISUALS_GBOX, "nex_bloom_exposure", "Night Mode", 100, 1, 100);
+local OriLua_LL_VISUALS_EXPOSURE = gui.Slider(OriLua_LL_VISUALS_GBOX, "OriLua_LL_VISUALS_BLOOM_EXPOSURE", "Night Mode", 100, 1, 100);
 local OriLua_LL_VISUALS_ASPECT_RATIO_SLIDER = gui.Slider(OriLua_LL_VISUALS_GBOX, "OriLua_LL_VISUALS_ASPECT_RATIO_SLIDER", "Aspect Ratio", 100, 1, 199)
+
+-------------------------------------- Checkbox + Sliders / HvH
 
 -------------------------------------- Description
 
@@ -60,7 +62,7 @@ callbacks.Register("Draw", "Night Mode", function()
 local controller = entities.FindByClass("CEnvTonemapController")[1];
 if(controller) then
 controller:SetProp("m_bUseCustomAutoExposureMin", 1);
-controller:SetProp("m_bUseCustomAutoExposureMax", 1);a
+controller:SetProp("m_bUseCustomAutoExposureMax", 1);
 controller:SetProp("m_flCustomAutoExposureMin", OriLua_LL_VISUALS_EXPOSURE:GetValue()/100);
 controller:SetProp("m_flCustomAutoExposureMax", OriLua_LL_VISUALS_EXPOSURE:GetValue()/100);
 end
