63598511f9e5aafd02e3fa3bce2f9d2000458936
LunarLuzog Aka ShiinaChan <54291915+LunarLuzogSM@users.noreply.github.com>
2020-03-08 11:20:47 +0100

    Update OriLua.lua

--- OriLua.lua
+++ OriLua.lua
@@ -18,7 +18,7 @@ local function Update()
     end
 end
 
--------------------------------------- Gui
+-------------------------------------- Gui ( ShiinaChan#5523 (Me))
 
 local OriLua_TAB = gui.Tab(gui.Reference("Settings"), "orilua.LL", "OriLua LL")
 local OriLua_LL_UPDATER_GBOX = gui.Groupbox(OriLua_TAB, "Updater", 10, 10, 160, 0) -- Updater-GBOX
@@ -27,21 +27,21 @@ local OriLua_LL_VISUALS_GBOX = gui.Groupbox(OriLua_TAB, "Visuals", 10, 190, 470,
 local OriLua_LL_RAINBOW_GBOX = gui.Groupbox(OriLua_TAB, "Rainbow", 10, 435, 470, 0) -- Rainbow-GBOX
 local OriLua_LL_MISCS_GBOX = gui.Groupbox(OriLua_TAB, "Miscs", 10, 675, 470, 0) -- Miscs-GBOX
 
--------------------------------------- Auto Update + Changelog
+-------------------------------------- Auto Update + Changelog ( ShiinaChan#5523 (Me))
 
 local OriLua_LL_CURRENTVERSION = gui.Text(OriLua_LL_UPDATER_GBOX, "Current version: v" .. CURRENTVERSION)
 local OriLua_LL_LATESTVERSION = gui.Text(OriLua_LL_UPDATER_GBOX, "Latest version: v" .. LATESTVERSION)
 local OriLua_LL_UPDATE = gui.Button(OriLua_LL_UPDATER_GBOX, "Update", Update)
 local OriLua_LL_CHANGELOG_TEXT = gui.Text(OriLua_LL_CHANGELOG_GBOX, http.Get("https://raw.githubusercontent.com/LunarLuzogSM/OriLua/master/changelog.txt"))
 
--------------------------------------- Checkbox + Sliders / Visuals
+-------------------------------------- Checkbox + Sliders / Visuals ( ShiinaChan#5523 (Me))
 
 local OriLua_LL_VISUALS_FULLBRIGHT = gui.Checkbox(OriLua_LL_VISUALS_GBOX, "OriLua_LL_VISUALS_FULLBRIGHT", "Full Bright", false)
 local OriLua_LL_VISUALS_ASPECT_RATIO = gui.Checkbox(OriLua_LL_VISUALS_GBOX, "OriLua_LL_VISUALS_ASPECT_RATIO", "Aspect Ratio", false);  
 local OriLua_LL_VISUALS_EXPOSURE_SLIDER = gui.Slider(OriLua_LL_VISUALS_GBOX, "OriLua_LL_VISUALS_BLOOM_EXPOSURE", "Night Mode", 100, 1, 100);
 local OriLua_LL_VISUALS_ASPECT_RATIO_SLIDER = gui.Slider(OriLua_LL_VISUALS_GBOX, "OriLua_LL_VISUALS_ASPECT_RATIO_SLIDER", "Aspect Ratio", 100, 1, 199)
 
--------------------------------------- Checkbox + Sliders / Rainbow
+-------------------------------------- Checkbox + Sliders / Rainbow ( ShiinaChan#5523 (Me))
 
 local OriLua_LL_RAINBOWEN_VIS_CHECKBOX = gui.Checkbox(OriLua_LL_RAINBOW_GBOX,"RainbowEnOvCh", "Enemy Visible", false);
 local OriLua_LL_RAINBOWEN_OCC_CHECKBOX = gui.Checkbox(OriLua_LL_RAINBOW_GBOX,"RainbowEnOvCh", "Enemy Occluded", false);
@@ -49,12 +49,11 @@ local OriLua_LL_RRAINBOWCR_CHECKBOX = gui.Checkbox(OriLua_LL_RAINBOW_GBOX,"Rainb
 local OriLua_LL_RAINBOW_TEXT = gui.Text(OriLua_LL_RAINBOW_GBOX, http.Get("https://raw.githubusercontent.com/LunarLuzogSM/OriLua/master/rainbow.txt"))
 -- local OriLua_LL_RAINBOW_SPEED = gui.Slider(OriLua_LL_RAINBOW_GBOX, "RainbowSp", "Rainbow Speed", 50, 0, 100)
 
--------------------------------------- Checkbox + Sliders / Miscs
+-------------------------------------- Checkbox + Sliders / Miscs ( ShiinaChan#5523 (Me))
 
 local OriLua_LL_MISCS_ERADAR = gui.Checkbox(OriLua_LL_MISCS_GBOX, "OriLua_LL_MISCS_ERADAR", "Engine Radar", false)
 
--------------------------------------- Description
-
+-------------------------------------- Description ( ShiinaChan#5523 (Me))
 
 OriLua_LL_VISUALS_EXPOSURE_SLIDER:SetDescription("100  Is  Lighter  Than  1")
 
@@ -120,7 +119,7 @@ if OriLua_LL_RAINBOWEN_VIS_CHECKBOX:GetValue() then
 end
 callbacks.Register( "Draw", "rainbowesp", rainbowesp);
     
------ Engine Radar
+----- Engine Radar ( KriZz87#5317 )
 
 local function engine_radar_draw()
 for index, Player in pairs(entities.FindByClass("CCSPlayer")) do
